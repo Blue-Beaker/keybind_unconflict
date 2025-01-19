590548ef68242410b7519c00075e3a5953221689
blue-beaker <yyy5566778899@126.com>
2025-01-19 18:20:39 +0800

    Fixed held modifiers preventing keybinds without modifiers from being activated

--- src/main/java/io/bluebeaker/keybindunconflict/mixin/MixinKeybinding.java
+++ src/main/java/io/bluebeaker/keybindunconflict/mixin/MixinKeybinding.java
@@ -4,6 +4,7 @@ import io.bluebeaker.keybindunconflict.KeyBindingsGetter;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.settings.KeyModifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,12 +14,26 @@ public abstract class MixinKeybinding {
 
     @Inject(method = "onTick(I)V",at=@At("HEAD"),cancellable = true)
     private static void onTick(int keyCode, CallbackInfo ci){
+        keybind_unconflict$tickKeyCode(keyCode);
+        ci.cancel();
+    }
+
+    @Unique
+    private static void keybind_unconflict$tickKeyCode(int keyCode) {
+        boolean pressed = false;
         for (KeyBinding keybinding : KeyBindingsGetter.getBindings(keyCode, KeyModifier.getActiveModifier())){
+            if (keybinding != null)
+            {
+                ((AccessorKeyBinding)keybinding).setPressTime(((AccessorKeyBinding)keybinding).getPressTime()+1);
+                pressed = true;
+            }
+        }
+        if(pressed) return;
+        for (KeyBinding keybinding : KeyBindingsGetter.getBindings(keyCode, KeyModifier.NONE)){
             if (keybinding != null)
             {
                 ((AccessorKeyBinding)keybinding).setPressTime(((AccessorKeyBinding)keybinding).getPressTime()+1);
             }
         }
-        ci.cancel();
     }
 }
