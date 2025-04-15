0c5619cb94c5e69a641bcc9428b2929cfa63af7f
blue-beaker <yyy5566778899@126.com>
2025-04-15 11:47:12 +0800

    Remove the useless config

--- src/main/java/io/bluebeaker/keybindunconflict/KeybindUnconflictConfig.java
+++ src/main/java/io/bluebeaker/keybindunconflict/KeybindUnconflictConfig.java
@@ -1,13 +1,8 @@
 package io.bluebeaker.keybindunconflict;
 
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.Config.LangKey;
-import net.minecraftforge.common.config.Config.Type;
-
-@Config(modid = KeybindUnconflict.MODID,type = Type.INSTANCE,category = "general")
+//@Config(modid = KeybindUnconflict.MODID,type = Type.INSTANCE,category = "general")
 public class KeybindUnconflictConfig {
-    @Comment("Example")
-    @LangKey("config.keybindunconflict.example.name")
-    public static boolean example = true;
+//    @Comment("Example")
+//    @LangKey("config.keybindunconflict.example.name")
+//    public static boolean example = true;
 }
